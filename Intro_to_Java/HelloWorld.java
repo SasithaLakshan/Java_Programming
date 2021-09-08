a4e9231df7296a211b435ca456a23ee3b687e610
Sasitha Lakshan <74831645+SasithaLakshan@users.noreply.github.com>
2021-09-08 05:54:09 +0530

    HelloWorld

---
+++ Intro_to_Java/HelloWorld.java
@@ -0,0 +1,9 @@
+//First Java Program :)
+
+package Intro_to_Java;
+
+class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Sasitha \nA guy with passion for Des + Dev :)");
+    }
+}
